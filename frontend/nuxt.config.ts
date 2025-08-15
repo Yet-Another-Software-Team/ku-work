af93cc7be1da08b959f14cc8354182784c06e9b9
Sirapob PHASUT <omegaooh@gmail.com>
2025-08-15 20:44:32 +0700

    feat: implement frontend that interact with Backend

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,13 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
 
   modules: [
-    '@nuxt/image',
-    '@nuxt/eslint',
-    '@nuxt/test-utils',
-    '@nuxt/scripts',
-    '@nuxt/ui'
-  ]
-})
+    "@nuxt/image",
+    "@nuxt/eslint",
+    "@nuxt/test-utils",
+    "@nuxt/scripts",
+    "@nuxt/ui",
+  ],
+  css: ["~/assets/css/main.css"],
+
+  runtimeConfig: {
+    public: {
+      apiBaseUrl: process.env.API_BASE_URL || "http://localhost:8000",
+    },
+  },
+});
