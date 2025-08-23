7c04f41e4965547f8e9e32460ec2a40c668db7b9
Phiranath PO-NGERN <phukaow18@gmail.com>
2025-08-23 16:17:12 +0700

    feat: add student profile page

---
+++ frontend/app/pages/student/profile/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+definePageMeta({
+	layout: "student",
+});
+</script>
+
+<template>
+    <div>
+        <h1>Student Profile</h1>
+        <p>Welcome to your profile page!</p>
+    </div>
+</template>
