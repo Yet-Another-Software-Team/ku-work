f9be8513e1dbba779c57f11941f4c4c3d4602efc
Sirapob PHASUT <omegaooh@gmail.com>
2025-08-25 21:43:40 +0700

    fix: Fix student registration URL and directory path

--- frontend/app/components/LoginCard.vue
+++ frontend/app/components/LoginCard.vue
@@ -27,7 +27,7 @@
                     Don't have an account?
                     <a
                         class="text-primary-600 font-semibold underline hover:text-primary-800"
-                        href="/register"
+                        href="students/register"
                     >
                         Sign Up
                     </a>
