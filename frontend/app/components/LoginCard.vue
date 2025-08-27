0268204d96cb2f8a9dae4d150856bfeae8f9484f
Sirapob PHASUT <omegaooh@gmail.com>
2025-08-27 10:28:17 +0700

    feat: change `/students/register` to `/register/students`
    BREAKING CHANGE: change `/students/register` to `/register/students`

--- frontend/app/components/LoginCard.vue
+++ frontend/app/components/LoginCard.vue
@@ -27,7 +27,7 @@
                     Don't have an account?
                     <a
                         class="text-primary-600 font-semibold underline hover:text-primary-800"
-                        href="students/register"
+                        href="/register/students"
                     >
                         Sign Up
                     </a>
