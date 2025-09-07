643e5ebd89303527834ddb7e15e0a3cf9987408a
Phiranath PO-NGERN <phukaow18@gmail.com>
2025-09-08 00:09:45 +0700

    style: add full height for the layout

--- frontend/app/layouts/admin.vue
+++ frontend/app/layouts/admin.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="block lg:flex light:bg-[#F7F8F4]">
         <SidebarComponent :is-admin="true" />
-        <div class="m-10 lg:w-full">
+        <div class="m-10 lg:w-full h-dvh">
             <slot />
         </div>
     </div>
