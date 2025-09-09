b54adc0a34d13c2fb3d678804e26474052ad5569
Phiranath PO-NGERN <phukaow18@gmail.com>
2025-09-10 02:45:01 +0700

    feat: set default admin and viewer property

--- frontend/app/layouts/admin.vue
+++ frontend/app/layouts/admin.vue
@@ -1,8 +1,19 @@
 <template>
     <div class="block lg:flex light:bg-[#F7F8F4]">
-        <SidebarComponent :is-admin="true" />
+        <SidebarComponent :is-admin="isAdmin" />
         <div class="m-10 lg:w-full h-full min-h-dvh">
             <slot />
         </div>
     </div>
 </template>
+
+<script setup lang="ts">
+withDefaults(
+    defineProps<{
+        isAdmin?: boolean;
+    }>(),
+    {
+        isAdmin: true,
+    }
+);
+</script>
--- frontend/app/layouts/viewer.vue
+++ frontend/app/layouts/viewer.vue
@@ -1,8 +1,19 @@
 <template>
     <div class="block lg:flex light:bg-[#F7F8F4]">
-        <SidebarComponent :is-viewer="false" />
+        <SidebarComponent :is-viewer="isViewer" />
         <div class="m-10 lg:w-full h-full min-h-dvh">
             <slot />
         </div>
     </div>
 </template>
+
+<script setup lang="ts">
+withDefaults(
+    defineProps<{
+        isViewer?: boolean;
+    }>(),
+    {
+        isViewer: false,
+    }
+);
+</script>
