5819a6f760e5ad7a1ed3b94fd673eb804a857b26
Phiranath PO-NGERN <phukaow18@gmail.com>
2025-09-10 11:17:05 +0700

    fix: remove not working property passing

--- frontend/app/layouts/admin.vue
+++ frontend/app/layouts/admin.vue
@@ -1,19 +1,8 @@
 <template>
     <div class="block lg:flex light:bg-[#F7F8F4]">
-        <SidebarComponent :is-admin="isAdmin" />
+        <SidebarComponent :is-admin="true" />
         <div class="m-10 lg:w-full h-full min-h-dvh">
             <slot />
         </div>
     </div>
 </template>
-
-<script setup lang="ts">
-withDefaults(
-    defineProps<{
-        isAdmin?: boolean;
-    }>(),
-    {
-        isAdmin: true,
-    }
-);
-</script>
--- frontend/app/layouts/viewer.vue
+++ frontend/app/layouts/viewer.vue
@@ -1,19 +1,8 @@
 <template>
     <div class="block lg:flex light:bg-[#F7F8F4]">
-        <SidebarComponent :is-viewer="isViewer" />
+        <SidebarComponent :is-viewer="false" />
         <div class="m-10 lg:w-full h-full min-h-dvh">
             <slot />
         </div>
     </div>
 </template>
-
-<script setup lang="ts">
-withDefaults(
-    defineProps<{
-        isViewer?: boolean;
-    }>(),
-    {
-        isViewer: false,
-    }
-);
-</script>
