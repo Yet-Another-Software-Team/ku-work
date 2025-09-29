f510b28e54ad9a27031e3f01e036dcc9c9c5d7a1
Sirapob PHASUT <omegaooh@gmail.com>
2025-09-29 14:07:36 +0700

    feat: add actual logo to main page and registration

--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -1,20 +1,19 @@
 <template>
     <div class="main-container px-[12vw] h-fit">
-        <div class="h-[8em] pt-2">
-            <Icon name="meteocons:umbrella-wind-alt" class="size-[8em] text-white" />
-        </div>
-        <div class="w-full flex flex-col md:flex-row justify-center items-center gap-[5em] pb-5">
+        <div
+            class="w-full flex flex-col md:flex-row justify-center items-center gap-[5em] pb-5 pt-[3em]"
+        >
             <div class="text-white">
                 <h1 class="text-5xl font-bold">
-                    Here in <span class="text-primary">KU-Work</span>,
+                    <img src="~/assets/images/base.png" class="h-[2em]" alt="KU Work" />
                 </h1>
-                <h2 class="text-3xl font-semibold mt-3">Great job awaits for you</h2>
-                <p class="text-base font-light mt-5 hidden lg:block">
+                <h2 class="text-3xl font-semibold text-shadow-lg mt-3">Great job awaits for you</h2>
+                <p class="text-base font-light mt-5 hidden text-shadow-lg lg:block">
                     A platform connecting KU's SKE/CPE students with<br />companies, offering direct
                     access to relevant tech-<br />jobs and internships.
                 </p>
             </div>
-            <LoginCard />
+            <LoginCard class="mt-[5em] shadow-lg" />
         </div>
     </div>
 </template>
@@ -33,7 +32,7 @@
     left: 0;
     width: 100%;
     height: 100%;
-    background-image: url("/images/background.png");
+    background-image: url("~/assets/images/background.png");
     background-size: cover;
     background-position: center;
     z-index: -1;
