a28e535dcc5de21c5cf839890a12b306ddff5a65
Phiranath PO-NGERN <phukaow18@gmail.com>
2025-10-01 09:33:07 +0700

    feat: make the nav bar a little responsive

--- frontend/app/pages/dashboard/application.vue
+++ frontend/app/pages/dashboard/application.vue
@@ -21,7 +21,7 @@
                 No job applications available.
             </div>
             <section class="h-[3em] overflow-hidden border-b-1 my-5">
-                <div class="flex flex-row gap-2 h-[6em] w-[40em] left-0 top-0">
+                <div class="flex flex-row gap-2 h-[6em] max-w-[40em] left-0 top-0">
                     <div
                         class="hover:cursor-pointer transition-all duration-150 text-center"
                         :class="setTailwindClasses('inprogress')"
@@ -140,13 +140,13 @@ function setTailwindClasses(activeCondition: string) {
     const condition = isSelected.value;
     if (condition == activeCondition) {
         if (condition == "inprogress") {
-            return "bg-yellow-200 flex flex-col border-1 rounded-3xl w-1/2 text-yellow-800 hover:bg-yellow-300";
+            return "bg-yellow-200 flex flex-col border-1 rounded-3xl w-1/3 text-yellow-800 hover:bg-yellow-300";
         } else if (condition == "accepted") {
-            return "bg-green-200 flex flex-col border-1 rounded-3xl w-1/2 text-primary-800 hover:bg-primary-300";
+            return "bg-green-200 flex flex-col border-1 rounded-3xl w-1/3 text-primary-800 hover:bg-primary-300";
         }
-        return "bg-error-200 flex flex-col border-1 rounded-3xl w-1/2 text-error-800 hover:bg-error-300";
+        return "bg-error-200 flex flex-col border-1 rounded-3xl w-1/3 text-error-800 hover:bg-error-300";
     } else {
-        return "bg-gray-200 flex flex-col border-1 rounded-3xl w-1/2 text-gray-500 hover:bg-gray-300";
+        return "bg-gray-200 flex flex-col border-1 rounded-3xl w-1/3 text-gray-500 hover:bg-gray-300";
     }
 }
 
