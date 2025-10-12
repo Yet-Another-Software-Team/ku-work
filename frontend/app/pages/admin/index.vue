27e8e8b61f23e1c1119c5308f0943c9ae28cff11
Sirapob PHASUT <omegaooh@gmail.com>
2025-10-12 13:05:14 +0700

    feat: admin login page

--- frontend/app/pages/admin/index.vue
+++ frontend/app/pages/admin/index.vue
@@ -1,7 +1,14 @@
 <template>
-    <div>
-        <p>This is the index that will be login page</p>
-        <p>So redirect to <a href="/admin/dashboard">dashboard</a></p>
+    <div class="w-screen flex-col items-center justify-center mt-5 overflow-hidden">
+        <img
+            src="~/assets/images/admin.png"
+            class="h-[4em] lg:h-[5em] w-full object-contain mb-10"
+            alt="KU Work Admin"
+        />
+        <div class="bg-white rounded-lg shadow-md p-6 w-[95vw] lg:w-[28em] mx-auto">
+            <h1 class="text-primary text-3xl font-bold mb-4 text-center">Admin Login</h1>
+            <AdminLoginField />
+        </div>
     </div>
 </template>
 
