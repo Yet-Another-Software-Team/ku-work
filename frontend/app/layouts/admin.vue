f4cbeb08eb2bd736b3a0df833d9e4f0f84bc5c9c
Sirapob PHASUT <omegaooh@gmail.com>
2025-11-19 22:13:22 +0700

    fix: padding outside of admin page footer

--- frontend/app/layouts/admin.vue
+++ frontend/app/layouts/admin.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="block lg:flex bg-[#F7F8F4] dark:bg-neutral-900">
         <SidebarComponent :is-admin="true" />
-        <div class="mx-10 lg:w-full h-full min-h-dvh flex flex-col">
-            <div class="flex-grow">
+        <div class="lg:w-full h-full min-h-dvh flex flex-col">
+            <div class="flex-grow mx-10">
                 <slot />
             </div>
             <LayoutFooter />
